8fbbf67794a8890131ec0b8d586d45b7f2867ad1
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2024-12-30 21:41:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ check_configs() {
 while true; do
     clear
     echo "============================================================================"
-    echo "    name BOT SETUP AND RUN SCRIPT"
+    echo "    Fintopio BOT SETUP AND RUN SCRIPT by @MeoMunDep"
     echo "============================================================================"
     echo
     echo "Current directory: $(pwd)"
@@ -112,7 +112,7 @@ while true; do
             else
                 print_green "Using node_modules from current directory"
             fi
-            node bot
+            cd fintopio && node bot
             read -p "Press Enter to continue..."
             ;;
         4)
